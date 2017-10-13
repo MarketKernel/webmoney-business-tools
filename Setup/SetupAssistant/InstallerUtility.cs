b8d2c53412e4857cbab3489f3bf92648cb8f7fde
Dmitry <global@marketkernel.com>
2017-10-13 06:25:48 +0300

    Инсталлер

--- Setup/SetupAssistant/InstallerUtility.cs
+++ Setup/SetupAssistant/InstallerUtility.cs
@@ -1,38 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using Microsoft.Build.Utilities;
+using System.Collections;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
 
 namespace SetupAssistant
 {
     [RunInstaller(true)]
     public partial class InstallerUtility : Installer
     {
+        private const string NGenFileName = "ngen.exe";
+
         public InstallerUtility()
         {
             InitializeComponent();
         }
-
-        public override void Commit(IDictionary savedState)
+        public override void Install(IDictionary stateSaver)
         {
-            base.Commit(savedState);
+            base.Install(stateSaver);
 
-            string runtimeDirectory = RuntimeEnvironment.GetRuntimeDirectory();
-            string ngenPath = Path.Combine(runtimeDirectory, "ngen.exe");
+            string frameworkDirectory = ToolLocationHelper.GetPathToDotNetFramework(
+                TargetDotNetFrameworkVersion.Version40,
+                Environment.Is64BitOperatingSystem
+                    ? DotNetFrameworkArchitecture.Bitness64
+                    : DotNetFrameworkArchitecture.Bitness32);
+
+
+            string ngenPath = Path.Combine(frameworkDirectory, NGenFileName);
+
+            if (!File.Exists(ngenPath))
+                return;
 
             var process = new Process
             {
                 StartInfo =
                 {
-                    FileName = ngenPath
+                    FileName = ngenPath,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true
                 }
             };
 
             string assemblyPath = Context.Parameters["assemblypath"];
+
+            if (string.IsNullOrEmpty(assemblyPath))
+                return;
+
             var assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+
+            if (string.IsNullOrEmpty(assemblyDirectory))
+                return;
+
             var dllPath = Path.Combine(assemblyDirectory, "WebMoney.Services.dll");
+
+            if (!File.Exists(dllPath))
+                return;
+
+            if (Context.Parameters.ContainsKey("Precompile"))
+            {
+                var precompile = Context.Parameters["Precompile"];
+
+                if (precompile.Equals("2"))
+                    return;
+            }
 
             process.StartInfo.Arguments = "install \"" + dllPath + "\"";
 
